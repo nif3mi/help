d7b21bc7f3dcf0dc87ddaac4e24c9fef2ea25857
Hassan Mutmahinat <hassanmutmahinat@gmail.com>
2022-09-15 20:45:20 +0100

    add button in a div

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// $(document).ready(function(){
+//     $("#one").click(function(){
+//         $("#card").toggle();
+//         $("#carb").hide();
+       
+//     })
+    
+//     $("#two").click(function(){
+//         $("#card").hide();
+//         $("#carb").toggle();
+//     })
+     
+//     $("#three").click(function(){
+//         $("#carb").hide();
+//         $("#cart").toggle();
+//     })
+     
+//     $("#four").click(function(){
+//         $("#cart").hide();
+//         $("#cars").toggle();
+//     })
+     
+//     $("#five").click(function(){
+//         $("#cars").hide();
+//         $("#carl").toggle();
+//     })
+
+// })
